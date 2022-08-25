3451c450b159016e2a9dfcdb9f07f50cc231f8b8
YoonsikKang <yoonsik.kang@cj.net>
2022-08-25 12:47:48 +0900

    Initial commit

---
+++ src/main/java/com/cj/quickspring/QuickSpringApplication.java
@@ -0,0 +1,13 @@
+package com.cj.quickspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cj/quickspring/QuickSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cj.quickspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
